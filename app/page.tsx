ff74386892258f47408dab0b45e7f16f4cd174ba
krishmarsonia <krishmarsonia@gmail.com>
2024-06-14 17:53:14 +0530

    time fields configured

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,194 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const largeCenter = "text-xl text-center";
+  const [realDate, setRealDate] = useState(new Date());
+  const [year, setYear] = useState(realDate.getFullYear());
+  const [month, setMonth] = useState(realDate.getMonth() + 1);
+  const [day, setDay] = useState(realDate.getDate());
+  const [hours, setHours] = useState(realDate.getHours());
+  const [minutes, setMinutes] = useState(realDate.getMinutes());
+  const [seconds, setSeconds] = useState(realDate.getSeconds());
+  const [milliseconds, setMilliseconds] = useState(realDate.getTime());
+  useEffect(() => {
+    setYear(realDate.getFullYear());
+    setMonth(realDate.getMonth() + 1);
+    setDay(realDate.getDate());
+    setHours(realDate.getHours());
+    setMinutes(realDate.getMinutes());
+    setSeconds(realDate.getSeconds());
+    setMilliseconds(realDate.getTime());
+  }, [year, month, day, hours, milliseconds, minutes, seconds, realDate]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex justify-center items-center flex-col mt-6">
+      <div>
+        <h1 className="text-5xl font-medium">Welcome to JS-Time</h1>
+        <h3 className={largeCenter}>Where you can easily deal with time.</h3>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="mt-5">
+        <p className={largeCenter}>Milliseconds</p>
+        <input
+          type="number"
+          name="milliseconds"
+          id="milliseconds"
+          className="text-black h-8 text-lg w-48"
+          value={milliseconds}
+          onChange={(e) => {
+            const milli = Number(e.target.value);
+            realDate.setTime(milli);
+            setRealDate(realDate);
+            setMilliseconds(milli);
+          }}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex w-1/2 justify-around items-center mt-5">
+        <div>
+          <p className={largeCenter}>Year</p>
+          <input
+            type="number"
+            name="years"
+            id="years"
+            className="text-black h-8 text-lg w-48"
+            value={year}
+            onChange={(e) => {
+              const yrs = Number(e.target.value);
+              if (yrs > 1969) {
+                realDate.setFullYear(yrs);
+                setRealDate(realDate);
+              }
+              setYear(yrs);
+            }}
+          />
+        </div>
+        <div>
+          <p className={largeCenter}>Month</p>
+          <input
+            type="text"
+            name="months"
+            id="months"
+            className="text-black h-8 text-lg w-48"
+            value={month}
+            onChange={(e) => {
+              const mth = Number(e.target.value);
+              if (mth > -1 && mth < 12) {
+                realDate.setMonth(mth);
+                setRealDate(realDate);
+              }
+              setMonth(mth);
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex w-1/2 justify-around items-center mt-5">
+        <div>
+          <p className={largeCenter}>Day</p>
+          <input
+            type="text"
+            name="day"
+            id="day"
+            className="text-black h-8 text-lg w-48"
+            value={day}
+            onChange={(e) => {
+              const dy = Number(e.target.value);
+              if (dy > 0 && dy < 32) {
+                realDate.setDate(dy);
+                setRealDate(realDate);
+              }
+              setDay(dy);
+            }}
+          />
+        </div>
+        <div>
+          <p className={largeCenter}>Hours</p>
+          <input
+            type="text"
+            name="hour"
+            id="hour"
+            className="text-black h-8 text-lg w-48"
+            value={hours}
+            onChange={(e) => {
+              const hrs = Number(e.target.value);
+              if (hrs > -1 && hrs < 24) {
+                realDate.setHours(hrs);
+              }
+              setHours(hrs);
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex w-1/2 justify-around items-center mt-5">
+        <div>
+          <p className={largeCenter}>Minutes</p>
+          <input
+            type="text"
+            name="minutes"
+            id="minutes"
+            className="text-black h-8 text-lg w-48"
+            value={minutes}
+            onChange={(e) => {
+              const mts = Number(e.target.value);
+              if (mts > -1 && mts < 60) {
+                realDate.setMinutes(mts);
+                setRealDate(realDate);
+              }
+              setMinutes(mts);
+            }}
+          />
+        </div>
+        <div>
+          <p className={largeCenter}>Seconds</p>
+          <input
+            type="text"
+            name="seconds"
+            id="seconds"
+            className="text-black h-8 text-lg w-48"
+            value={seconds}
+            onChange={(e) => {
+              const sec = Number(e.target.value);
+              if (sec > -1 && sec < 60) {
+                realDate.setSeconds(sec);
+              }
+              setSeconds(sec);
+            }}
+          />
+        </div>
+      </div>
+      <div className="mt-9">
+        <p>
+          The entered date is{" "}
+          <span className="font-semibold">
+            {realDate.getDate()} {realDate.getMonth() + 1}{" "}
+            {realDate.getFullYear()}
+          </span>
+          .
+        </p>
+        <p>
+          The day of the week is{" "}
+          <span className="font-semibold">{realDate.getDay()}</span>.
+        </p>
+        <p>
+          The current Time is{" "}
+          <span>
+            {(realDate.getHours() > 12
+              ? realDate.getHours() - 12
+              : realDate.getHours()) < 10
+              ? "0" +
+                (realDate.getHours() > 12
+                  ? realDate.getHours() - 12
+                  : realDate.getHours())
+              : realDate.getHours() > 12
+              ? realDate.getHours() - 12
+              : realDate.getHours()}
+            :
+            {realDate.getMinutes() < 10
+              ? "0" + realDate.getMinutes()
+              : realDate.getMinutes()}
+            :{realDate.getSeconds()} {realDate.getHours() > 12 ? "PM" : "AM"}
+          </span>
+          .
+        </p>
       </div>
     </main>
   );
